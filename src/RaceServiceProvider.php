b4827dff43d3a033478719821d4986d3fcb06352
saeed ebrahimzade <s.ebrahimzade90@gmail.com>
2024-02-29 16:47:08 +0330

    add ServiceProvider & orchestra/testbench

---
+++ src/RaceServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SavvyTech\Race;
+
+use Illuminate\Support\ServiceProvider;
+use SavvyTech\Race\Console\RaceCommand;
+
+class RaceServiceProvider extends ServiceProvider
+{
+	public function register()
+	{
+
+	}
+
+	public function boot()
+	{
+		//
+	}
+}
